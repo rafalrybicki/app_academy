3c0c30047342eda07fc65b1a433a895f597d9b86
Rafał Rybicki <rafarybicki@gmail.com>
2020-01-31 13:18:34 +0100

    Advanced Methods Exercise

---
+++ 1 advanced_methods_exercise/01_coprime.rb
@@ -0,0 +1,20 @@
+# Write a method, coprime?(num_1, num_2), that accepts two numbers as args.
+# The method should return true if the only common divisor between the two numbers is 1.
+# The method should return false otherwise. For example coprime?(25, 12) is true because
+# 1 is the only number that divides both 25 and 12.
+
+def coprime?(num1, num2)
+
+    smaller_num = num1 <num2 ? num1 : num2
+    
+    (2...smaller_num).each do |i|
+        return false if num1 % i == 0 && num2 % i == 0
+    end
+
+    true
+end
+
+p coprime?(25, 12)    # => true
+p coprime?(7, 11)     # => true
+p coprime?(30, 9)     # => false
+p coprime?(6, 24)     # => false
